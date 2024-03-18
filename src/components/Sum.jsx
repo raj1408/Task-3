f9c962b0b245a7bd6474b67fd6e7fb81f297971b
Raj Saini <101448363+raj1408@users.noreply.github.com>
2024-03-18 16:50:15 +0530

    Update Sum.jsx

--- src/components/Sum.jsx
+++ src/components/Sum.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Sum(props) {
   return (
     <div className="sumContainer">
+      <button onClick={props.toggleButton}>{props.buttonText}</button>
       <p className="heading">{props.text}</p>
       <input
         type="text"
@@ -14,7 +15,9 @@ export default function Sum(props) {
         placeholder="Operand 2"
         onChange={(e) => props.setOperandTwo(e.target.value)}
       />
-      <button onClick={props.handleSum}>Add Operands</button>
+      <button onClick={props.handleSum} disabled={!props.buttonEnabled}>
+        Add Operands
+      </button>
       <p className="sum">{props.sum}</p>
     </div>
   );
